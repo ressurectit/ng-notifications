7e2eb176f089dd7082b7e650ffd1e41a496b1e18
kukjevov <kukjevov@gmail.com>
2017-03-28 09:05:04 +0200

    UPDATE - added support for load options using module

--- src/modules/notifications.module.ts
+++ src/modules/notifications.module.ts
@@ -1,10 +1,11 @@
 import {CommonModule} from '@angular/common';
-import {NgModule, ModuleWithProviders} from '@angular/core';
+import {NgModule, ModuleWithProviders, FactoryProvider} from '@angular/core';
 
 import {GlobalNotificationsService} from '../common/notifications.service';
 import {Notifications} from '../common/notifications.component';
 import {GlobalNotifications} from '../common/global-notifications.component';
 import {NotificationMessage} from '../common/notification.message.component';
+import {NotificationsOptions} from '../common/notifications.options';
 
 /**
  * Module for notifications
@@ -27,6 +28,26 @@ export class NotificationsModule
         return {
             ngModule: NotificationsModule,
             providers: [GlobalNotificationsService]
+        };
+    }
+
+    /**
+     * Returns module with global notifications providers that is used for app root module and options
+     * @param {() => NotificationsOptions} options Options factory
+     */
+    public static forRootWithOptions(options: () => NotificationsOptions): ModuleWithProviders 
+    {
+        return {
+            ngModule: NotificationsModule,
+            providers: 
+            [
+                GlobalNotificationsService,
+                <FactoryProvider>
+                {
+                    provide: NotificationsOptions,
+                    useFactory: options
+                }
+            ]
         };
     }
 }

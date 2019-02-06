d2b363e25da44873ac3fa433bdc1e8b8f065e723
kukjevov <kukjevov@gmail.com>
2019-02-06 07:59:22 +0100

    INT: updated entry index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-export {GlobalNotificationsService, LocalNotificationsService} from './common/notifications.service';
-export {Notifications} from './common/notifications.component';
-export {GlobalNotifications} from './common/global-notifications.component';
-export {NotificationMessage} from './common/notification.message.component';
-export {Notification} from './common/notification';
-export {NotificationType} from './common/notification.type';
-export {NotificationsOptions} from './common/notifications.options';
+export * from './common';
+export * from './components';
+export * from './directives';
 export {NotificationsModule} from './modules/notifications.module';
